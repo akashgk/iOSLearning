9098b6ba40bf552d080a366334983322daaa8f4c
Akash G Krishnan <akash.krishnan@fullthrottlelabs.com>
2021-09-03 23:55:36 +0530

    timer

--- EggTimer-iOS13/EggTimer/ViewController.swift
+++ EggTimer-iOS13/EggTimer/ViewController.swift
@@ -10,16 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
     let eggTimes : [String : Int] = [
-        "Soft" : 5,
+        "Soft" : 3,
         "Medium" : 8,
-        "Hard" : 12
+        "Hard" : 17
     ]
-
+    
+    @IBOutlet weak var pageTitle: UILabel!
+    
+    @IBOutlet weak var progressbar: UIProgressView!
+    
+    var secondsRemaining = 60
+    var timer = Timer()
+     
     
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
+        timer.invalidate()
+        progressbar.progress = 0.0
         let hardness = sender.currentTitle!;
-        print(eggTimes[hardness]!)
+        secondsRemaining = eggTimes[hardness]!
+
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil,  repeats: true)
+    }
+    
+    @objc func updateTimer(){
+        if(secondsRemaining > 0){
+            secondsRemaining -= 1
+            pageTitle.text = "\(secondsRemaining)"
+        } else{
+            timer.invalidate()
+            progressbar.progress = 1.0
+            pageTitle.text = "Done!"
+        }
     }
     
 }
