a7e786e351f44d0602893088c1b8a96a0ceb90db
Akash G Krishnan <akash.krishnan@fullthrottlelabs.com>
2021-09-04 23:17:19 +0530

    quiz app

--- EggTimer-iOS13/EggTimer/ViewController.swift
+++ EggTimer-iOS13/EggTimer/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import AVFoundation
+var player: AVAudioPlayer!
 
 class ViewController: UIViewController {
     let eggTimes : [String : Int] = [
@@ -19,28 +21,42 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var progressbar: UIProgressView!
     
-    var secondsRemaining = 60
+    var secondsPassed = 0
+    var totalTime = 0
     var timer = Timer()
-     
-    
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         timer.invalidate()
         progressbar.progress = 0.0
         let hardness = sender.currentTitle!;
-        secondsRemaining = eggTimes[hardness]!
-
+        totalTime = eggTimes[hardness]!
+        
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil,  repeats: true)
     }
     
     @objc func updateTimer(){
-        if(secondsRemaining > 0){
-            secondsRemaining -= 1
-            pageTitle.text = "\(secondsRemaining)"
+        if(secondsPassed < totalTime){
+            pageTitle.text = "\(totalTime - secondsPassed)"
+            secondsPassed += 1
+            let progressPercent = Float(secondsPassed) / Float(totalTime)
+            progressbar.progress = progressPercent
         } else{
             timer.invalidate()
-            progressbar.progress = 1.0
             pageTitle.text = "Done!"
+            secondsPassed = 0
+            totalTime = 0
+            playAlarmSound()
+        }
+    }
+    
+    func playAlarmSound() {
+        guard let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3") else { return }
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            player!.play()
+        } catch let error {
+            print(error.localizedDescription)
         }
     }
     
